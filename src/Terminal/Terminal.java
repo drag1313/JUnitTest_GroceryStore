845e27a0e44d8b878581b20f667678bc4385195c
drag13 <nereality13@mail.ru>
2018-11-03 10:03:34 +0300

    all work.nice)

--- src/Terminal/Terminal.java
+++ src/Terminal/Terminal.java
@@ -6,6 +6,9 @@ public class Terminal {
 
     int totalPrice = 0;
     int price = 0;
+    int countMilk = 0;
+    int countChips = 0;
+    int countCola = 0;
 
     WeakHashMap<String, Integer> products = new WeakHashMap<String, Integer>();
 
@@ -15,15 +18,35 @@ public class Terminal {
     }
 
 
-    public int purchase() {
-
-
-        return price;
+    public void purchase(String productName) {
+        if (products.containsKey(new String(productName))) {
+            price = products.get(productName);
+            totalPrice += price;
+        }
+        switch (productName) {
+            case "Молоко":
+                countMilk = countMilk + 1;
+                break;
+            case "Кола":
+                countCola = countCola + 1;
+                break;
+            case "Чипсы":
+                countChips = countChips + 1;
+                break;
+        }
     }
 
     public int calculate() {
-
-
+        if (countMilk > 2) {
+            totalPrice = totalPrice - ((countMilk / 3) * 31);
+        }
+        if (countChips > 3) {
+            totalPrice = totalPrice - ((countChips / 4) * 31);
+        }
+        if (countCola > 1) {
+            totalPrice = totalPrice - ((countCola / 2) * 39);
+        }
         return totalPrice;
     }
 }
+
--- src/Terminal/TerminalTest.java
+++ src/Terminal/TerminalTest.java
@@ -1,5 +1,8 @@
 package Terminal;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,18 +10,56 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TerminalTest {
 
-    @BeforeEach
-    void setUp() {
-        Terminal t = new Terminal();
+    private Terminal t;
+
+    @Test
+    public void calculate1() {
+        t = new Terminal();
         t.setPrice("Молоко", 70);
         t.setPrice("Шампунь", 200);
         t.setPrice("Чипсы", 40);
         t.setPrice("Шоколад", 50);
         t.setPrice("Кола", 89);
+        t.purchase("Молоко");
+        t.purchase("Шампунь");
+        t.purchase("Чипсы");
+        t.purchase("Шоколад");
+        t.purchase("Кола");
+        t.purchase("Молоко");
+        t.purchase("Кола");
+        t.purchase("Молоко");
+        assertEquals(608, t.calculate());
     }
 
     @Test
-    void calculate() {
+    public void calculste2() {
+        t = new Terminal();
+        t.setPrice("Молоко", 70);
+        t.setPrice("Шампунь", 200);
+        t.setPrice("Чипсы", 40);
+        t.setPrice("Шоколад", 50);
+        t.setPrice("Кола", 89);
+        t.purchase("Чипсы");
+        t.purchase("Чипсы");
+        t.purchase("Чипсы");
+        t.purchase("Чипсы");
+        t.purchase("Чипсы");
+        assertEquals(169, t.calculate());
+    }
 
+    @Test
+    public void calculate3() {
+        t = new Terminal();
+        t.setPrice("Молоко", 70);
+        t.setPrice("Шампунь", 200);
+        t.setPrice("Чипсы", 40);
+        t.setPrice("Шоколад", 50);
+        t.setPrice("Кола", 89);
+        t.purchase("Молоко");
+        t.purchase("Шампунь");
+        t.purchase("Чипсы");
+        t.purchase("Шоколад");
+        t.purchase("Кола");
+        assertEquals(449, t.calculate());
     }
 }
